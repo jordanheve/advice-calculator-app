c35005f65f7c0c94807473e030ddb0022ce05752
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2023-07-10 16:56:45 -0600

    added full website code

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+function App() {
+
+  const [advice, setAdvice] = useState({})
+  const [fadeClass, setFadeClass] = useState(" fadeIn")
+
+  let handleAdvice = ()=> {
+    fetch('https://api.adviceslip.com/advice', {cache: 'no-cache'})
+      .then(response => response.json())
+      .then((data) =>
+				setAdvice({ number: data.slip.id, advice: data.slip.advice })
+			)
+  }
+
+  let handleClick = () => {
+    setFadeClass(" fadeOut")
+    setTimeout(() => {
+      handleAdvice()
+    }, 1000
+
+    )
+      setTimeout(()=> (setFadeClass(" fadeIn")),1500)
+    }
+  
+    useEffect(() => {
+      handleAdvice()
+    }, [])
+
+  return (
+    <>
+      <main className="main animated fadeIn">
+        <div className="advice-container">
+          <h5 className={'advice-container__number animated'+ fadeClass} >Advice #{advice.number}</h5>
+          <q className={'advice-container__advice animated'+ fadeClass}>{advice.advice}</q>
+        </div>
+        <div className="line-divisor"></div>
+        <button className="advice-btn" onClick={handleClick}>
+          <i className="advice-btn__dice-icon"></i>
+        </button>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <footer className='footer'>
+    Coded by <br/>
+      <a
+        href="https://www.frontendmentor.io/profile/jordanheve"
+        target="_blank"
+        rel='noreferrer'
+        > JordanHeVe</a>
+    </footer>
+  </>
+
+)
